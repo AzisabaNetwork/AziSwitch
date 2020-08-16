ed5c9845e18cfc25d86440c1dcca6a6db80fcec3
Mori01231 <mori01231mc@gmail.com>
2020-08-16 20:37:24 +0900

    [Add] Checking first argument to determine mode.

--- src/main/java/com/github/mori01231/aziswitch/NewSwitchGroupCommandExecutor.java
+++ src/main/java/com/github/mori01231/aziswitch/NewSwitchGroupCommandExecutor.java
@@ -4,9 +4,23 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.util.List;
+
 public class NewSwitchGroupCommandExecutor implements CommandExecutor {
+
+    List<String> singleServerGroups = AziSwitch.getInstance().getConfig().getStringList("SingleServerGroups");
+    List<String> allServerGroups = AziSwitch.getInstance().getConfig().getStringList("AllServerGroups");
+
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+        if(args[0].equalsIgnoreCase("single")){
+            List<String> singleServerGroups = AziSwitch.getInstance().getConfig().getStringList("SingleServerGroups");
+        }
+        if(args[0].equalsIgnoreCase("all")){
+            List<String> allServerGroups = AziSwitch.getInstance().getConfig().getStringList("AllServerGroups");
+        }
+
+        return true;
     }
 }
