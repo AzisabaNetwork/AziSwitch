f14b604fdb5fcb6df88a97b854f307efaafc7cec
Mori01231 <mori01231mc@gmail.com>
2020-08-16 14:17:23 +0900

    [Remove] Unnecessary code

--- src/main/java/com/github/mori01231/aziswitch/SwitchCommandExecutor.java
+++ src/main/java/com/github/mori01231/aziswitch/SwitchCommandExecutor.java
@@ -62,59 +62,6 @@ public class SwitchCommandExecutor implements CommandExecutor {
                 SwitchFromMember(player, group);
             }
 
-/*
-            //member to builder
-            if (sender.hasPermission("aziswitch.switchbuilder") && !sender.hasPermission("aziswitch.isbuilder")){
-
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add builder server=" + servername);
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3Builderモードになりました。" ));
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove switchbuilder server=" + servername);
-            }
-
-            //member to moderator
-            if (sender.hasPermission("aziswitch.switchmoderator") && !sender.hasPermission("aziswitch.ismoderator")){
-
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add moderator server=" + servername);
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3Moderatorモードになりました。" ));
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove switchmoderator server=" + servername);
-            }
-
-            //member to admin
-            if (sender.hasPermission("aziswitch.switchadmin") && !sender.hasPermission("aziswitch.isadmin")){
-
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add admin server=" + servername);
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3Adminモードになりました。" ));
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove switchadmin server=" + servername);
-            }
-
-            //member to adminmember
-            if (sender.hasPermission("aziswitch.switchadminmember") && !sender.hasPermission("aziswitch.isadminmember")){
-
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add adminmember server=" + servername);
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3AdminMemberモードになりました。" ));
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove switchadminmember server=" + servername);
-            }
-
-            //member to developer
-            if (sender.hasPermission("aziswitch.switchdeveloper") && !sender.hasPermission("aziswitch.isdeveloper")){
-
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add developer");
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3Developerモードになりました。" ));
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove switchdeveloper");
-            }
-
-            //member to owner
-            if (sender.hasPermission("aziswitch.switchowner") && !sender.hasPermission("aziswitch.isowner")){
-
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add owner");
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3Ownerモードになりました。" ));
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove switchowner");
-
-            }
-*/
-
-
-
             //CHANGE TO MEMBER MODE
 
             //builder to member
